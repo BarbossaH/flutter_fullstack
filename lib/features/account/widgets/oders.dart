d42d47ba9f11da8c29a0982bd7b918870aac5b02
huangbo <huangbobeijing2008@163.com>
2023-10-11 11:15:29 +1300

    add user address in home page

--- lib/features/account/widgets/oders.dart
+++ lib/features/account/widgets/oders.dart
@@ -15,7 +15,7 @@ class _OrdersState extends State<Orders> {
     'https://images.unsplash.com/photo-1696257203553-20ada15fce65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60',
     'https://images.unsplash.com/photo-1696247833485-bcd2014cbdb9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60',
     'https://images.unsplash.com/photo-1695990190649-4df3277b3be6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-    'https://images.unsplash.com/photo-1695990190649-4df3277b3be6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1696263075473-49877750c1a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
   ];
   @override
   Widget build(BuildContext context) {
@@ -48,7 +48,7 @@ class _OrdersState extends State<Orders> {
         ),
         Container(
           height: 170,
-          padding: EdgeInsets.only(left: 10, top: 20),
+          padding: const EdgeInsets.only(left: 10, top: 20, right: 0),
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) {
