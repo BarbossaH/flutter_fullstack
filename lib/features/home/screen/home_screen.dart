c19b53b53b12f53beb90f1eadc357c7a51c567f6
huangbo <huangbobeijing2008@163.com>
2023-10-11 11:49:37 +1300

    add category images

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:amazone_clone/constants/global_variables.dart';
+import 'package:amazone_clone/features/home/widgets/top_categories.dart';
 import 'package:amazone_clone/features/home/widgets/user_address.dart';
 // import 'package:amazone_clone/providers/user_provider.dart';
 import 'package:flutter/material.dart';
@@ -90,7 +91,8 @@ class _HomeScreenState extends State<HomeScreen> {
             UserAddress(),
             SizedBox(
               height: 10,
-            )
+            ),
+            TopCategories(),
           ],
         ));
   }
--- lib/features/home/widgets/top_categories.dart
+++ lib/features/home/widgets/top_categories.dart
@@ -1,3 +1,4 @@
+import 'package:amazone_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
 class TopCategories extends StatelessWidget {
@@ -5,6 +6,35 @@ class TopCategories extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      height: 60,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: GlobalVariable.categoryImages.length,
+          itemExtent: 75,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: Image.asset(
+                      GlobalVariable.categoryImages[index]['image']!,
+                      fit: BoxFit.cover,
+                      height: 40,
+                      width: 40,
+                    ),
+                  ),
+                ),
+                Text(
+                  GlobalVariable.categoryImages[index]['title']!,
+                  style: const TextStyle(
+                      fontSize: 12, fontWeight: FontWeight.w400),
+                )
+              ],
+            );
+          }),
+    );
   }
 }
